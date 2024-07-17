79303acc9567f9b103b085af4688d5c9b6b28cc6
KimInteger <kjs20151240@gmail.com>
2024-07-17 10:17:10 +0900

    test_Integer : ubuntu install node

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-console.log('Hellow World!')
-
-console.log('안녕 호진아');
-
+const http = require('http');
+const port = process.env.port || 3000;
+const server = http.createServer((req,res)=>{
+console.log('hello nodejs to Linux Ubuntu');
+});
+server.listen(port,(err)=>{
+if(err){
+console.error('error');
+}
+console.log(`http://localhost:${port}`);
+console.log('listen to my heart');
+});
